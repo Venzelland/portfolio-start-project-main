db88f07120bb3be96ea2df857f5226fec8630518
Venzelland <pavielvolodko1997@mail.ru>
2023-10-25 19:12:26 +0300

    last lesson

--- src/layout/sections/works/Works_Styles.ts
+++ src/layout/sections/works/Works_Styles.ts
@@ -15,8 +15,8 @@ const Works = styled.section`
 
 const Work = styled.div`
   background-color: ${theme.colors.secondaryBg};
-  width: 330px;
-  flex-grow: 1;
+  //width: 330px;
+  //flex-grow: 1;
 
   ${Link} {
     padding: 10px 0;
@@ -27,7 +27,7 @@ const Work = styled.div`
   }
 
   @media ${theme.media.desktop} {
-    max-width: 540px;
+    //max-width: 540px;
   }
 
 
